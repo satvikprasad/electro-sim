f8ec5274cfb6097bff39de8c4cc14052d64a71b0
Satvik Prasad <satvikprasad@gmail.com>
2023-12-22 15:53:12 +1100

    Fix build script

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-SET include=-Ilib\raylib\src
-SET linker=lib\raylib\src\libraylib.a -lgdi32 -lole32 -loleaut32 -limm32 -lwinmm
+SET include=-Iraylib\src
+SET linker=raylib\src\libraylib.a -lgdi32 -lole32 -loleaut32 -limm32 -lwinmm
 
-gcc src\*.c -o main.exe %include% %linker%
+gcc main.c -o main.exe %include% %linker%
